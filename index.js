adc8da0e34eb23d54491461717cb345e6186b0f9
aravindhselvam3 <130632860+aravindhselvam3@users.noreply.github.com>
2024-02-07 06:36:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv').config();
 const cors = require('cors');
 
-dotenv.config();
 const app = express();
 //use express.json() to get data into json format
 app.use(express.json());
